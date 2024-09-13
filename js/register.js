1018da103f61e6fc70a4c0603b4a751357af5458
cole92 <aleksandar9212@gmail.com>
2024-09-13 07:15:08 +0200

    login.js finished

--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import app from "./config.js";
 
@@ -6,7 +6,7 @@ const database = getDatabase(app);
 
 const auth = getAuth();
 const signUp = document.getElementById('signUp');
-
+// Listener na signUp dugme
 signUp.addEventListener('click', es => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -16,10 +16,18 @@ signUp.addEventListener('click', es => {
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    // ...
+    // Kreiramo korisnike u bazi kao objekte, koristimo metode firebase-a
+    set(ref(database, 'users/' + user.uid), {
+      username: username,
+      email: email
+    })
 
     alert('User is created');
-    location.href = '../login.html'; // Nisam siguran za putanju! (Izmeniti naknadno)
+    setTimeout(() => {
+      console.log(location.href);
+      window.location = 'login.html';
+    }, 1000)
+   
   })
   .catch((error) => {
     const errorCode = error.code;
